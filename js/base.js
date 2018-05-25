8cddd1cae9747d2cdb466de9eaf9d4897171e911
m1ndgap <posseslash@gmail.com>
2018-05-25 17:25:23 +0200

    clicking prototype is ready

--- js/base.js
+++ js/base.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
   $('.show-stats').on('click', function(){
     $(this).next().toggle();
   });
+
+  $('.show-perks').on('click', function(){
+    $(this).next().toggle();
+  });
+
+  $('.show-links').on('click', function(){
+    $(this).next().toggle();
+  });
 });
