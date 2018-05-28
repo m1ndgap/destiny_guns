3fe14c051091081b461258433e58369e482b2575
m1ndgap <posseslash@gmail.com>
2018-05-28 16:52:05 +0200

    developing node in progress

--- sleeper_nodes/sleeper_nodes.js
+++ sleeper_nodes/sleeper_nodes.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
-  $(function(){
-         $.getJSON('sleeper_nodes.json',function(data){
-             console.log('success');
-             alert(data[0].id);
-             $.each(data.id,function(i,ide){
-                 $('ul').append('<li>'+ide.firstName+' '+ide.lastName+'</li>');
-             });
-         }).error(function(){
-             console.log('error');
-         });
-     });
+  // $(function(){
+  //        $.getJSON('sleeper_nodes.json',function(data){
+  //            console.log('success');
+  //            alert(data[0].id);
+  //            $.each(data.id,function(i,ide){
+  //                $('ul').append('<li>'+ide.firstName+' '+ide.lastName+'</li>');
+  //            });
+  //        }).error(function(){
+  //            console.log('error');
+  //        });
+  //    });
+
+  $('.sn-check').on('click', 'input', function(){
+    var row = $(this).closest('.row')
+
+    if(row.hasClass('.completed-node')) {
+      row.appendTo('.sn-in-progress');
+      row.removeClass('.completed-node');
+      }
+      else {
+      row.appendTo('.sn-completed');
+      row.addClass('.completed-node');
+      }
+  });
 });
