5e8a7ca74f8971d4b44df9910e4c650dca564412
m1ndgap <posseslash@gmail.com>
2018-05-29 14:05:29 +0200

    getting jsonp to remote server

--- sleeper_nodes/sleeper_nodes.js
+++ sleeper_nodes/sleeper_nodes.js
@@ -1,26 +1,29 @@
 $(document).ready(function() {
-  // $(function(){
-  //        $.getJSON('sleeper_nodes.json',function(data){
-  //            console.log('success');
-  //            alert(data[0].id);
-  //            $.each(data.id,function(i,ide){
-  //                $('ul').append('<li>'+ide.firstName+' '+ide.lastName+'</li>');
-  //            });
-  //        }).error(function(){
-  //            console.log('error');
-  //        });
-  //    });
+  $(function(){
+    $.getJSON( "sleeper_nodes.json", function( data ) {
+      var items = [];
+      $.each( data, function( key, val ) {
+
+      });
+
+      $( "<ul/>", {
+        "class": "my-new-list",
+        html: items.join( "" )
+      }).appendTo( "body" );
+    });
 
   $('.sn-check').on('click', 'input', function(){
     var row = $(this).closest('.row')
 
     if(row.hasClass('.completed-node')) {
-      row.appendTo('.sn-in-progress');
-      row.removeClass('.completed-node');
+        // row.animate()
+        row.appendTo('.sn-in-progress');
+        row.removeClass('.completed-node');
       }
       else {
-      row.appendTo('.sn-completed');
-      row.addClass('.completed-node');
+        row.prependTo('.sn-completed');
+        row.addClass('.completed-node');
       }
   });
+});
 });
