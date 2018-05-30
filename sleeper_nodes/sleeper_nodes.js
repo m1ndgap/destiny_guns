fb2571f873ee9c7b7dc5901bfc9affeeed42e680
m1ndgap <posseslash@gmail.com>
2018-05-30 11:35:24 +0200

    local storage functionality working on sleeper nodes

--- sleeper_nodes/sleeper_nodes.js
+++ sleeper_nodes/sleeper_nodes.js
@@ -3,18 +3,16 @@ $(document).ready(function() {
       //../sleeper_nodes/sleeper_nodes.jsonp
 
   if(localStorage.getItem('nodes'))
-    {
-      console.log("чччччччччччччч11111111");
-    return;
-    }
+    {}
     else
     {
-      console.log("чччччччччччччч");
-      var ls_nodes = Array();
+
+      var ls_nodes = [];
       localStorage.setItem('nodes', JSON.stringify(ls_nodes));
       console.log(ls_nodes);
    };
-console.log("чччччччччччччч");
+
+
   $.ajax({
      url: "sleeper-nodes.jsonp",
      type: 'POST',
@@ -30,26 +28,33 @@ console.log("чччччччччччччч");
    });
 
 
-console.log("чччччччччччччч");
+//console.log("чччччччччччччч");
+
   $('.sn-check').on('click', 'input', function(){
     var row = $(this).closest('.row');
     var node_id = $(this).data('id');
+    var ls_nodes = JSON.parse(localStorage.getItem('nodes'));
 
     if(row.hasClass('.completed-node')) {
         // row.animate()
+        var index = ls_nodes.indexOf(node_id);
+          if (index > -1) {
+            ls_nodes.splice(index, 1);
+          }
         row.appendTo('.sn-in-progress');
         row.removeClass('.completed-node');
+        localStorage.setItem('nodes', JSON.stringify(ls_nodes));
+        console.log("local storage contains:" + localStorage.getItem('nodes'));
       }
       else {
-        var ls_nodes = JSON.parse(localStorage.getItem('nodes'));
         row.prependTo('.sn-completed');
         row.addClass('.completed-node');
-        console.log('Type of ls nodes is:' + typeof(ls_nodes));
+        ls_nodes.push(node_id);
+        console.log('Type of ls_nodes is:' + typeof(ls_nodes));
         console.log("ls_nodes is:" + ls_nodes);
         console.log("Node_id is:" + node_id);
-        // ls_nodes.push(node_id);
-        // localStorage.setItem('nodes', ls_nodes);
-        // console.log(localStorage.getItem('nodes'));
+        localStorage.setItem('nodes', JSON.stringify(ls_nodes));
+        console.log("local storage contains:" + localStorage.getItem('nodes'));
       }
   });
 });
