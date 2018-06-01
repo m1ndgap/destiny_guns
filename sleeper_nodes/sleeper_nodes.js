0e897e4efd8c7bda6993b383aa35e636cd9d0e6e
m1ndgap <posseslash@gmail.com>
2018-06-01 14:28:52 +0200

    latest working version of sleeper nodes

--- sleeper_nodes/sleeper_nodes.js
+++ sleeper_nodes/sleeper_nodes.js
@@ -12,39 +12,41 @@ $(document).ready(function() {
     };
 
 
-  $.ajax({
-     url: "sleeper-nodes.jsonp",
-     type: 'POST',
-     crossDomain: true,
-     dataType: 'jsonp',
-     accepts: 'application/jsonp',
-     jsonpCallback: 'callback',
-       success: function (data) {
-           console.log(data);
-           var json = data;
-           var localstorage = JSON.parse(localStorage.getItem('nodes'));
-           $.each(json, function(index, value){
-              var id = json[index].id;
-              var name = json[index].name;
-              var desc = json[index].description;
-              var video = json[index].video;
-              if (localstorage.includes(id)) {
-                var target = '.sn-completed';
-                var checked = 'checked';
-              } else {
-                var target = '.sn-in-progress';
-                var checked = '';
-              };
+    $.ajax({
+        url: "sleeper-nodes.jsonp",
+        type: 'POST',
+        crossDomain: true,
+        dataType: 'jsonp',
+        accepts: 'application/jsonp',
+        jsonpCallback: 'callback',
+          success: function (data) {
+              console.log(data);
+              var json = data;
+              var localstorage = JSON.parse(localStorage.getItem('nodes'));
+              $.each(json, function(index, value){
+                 var id = json[index].id;
+                 var name = json[index].name;
+                 var desc = json[index].description;
+                 var video = json[index].video;
+                 if (localstorage.includes(id)) {
+                   var target = '.sn-completed';
+                   var checked = 'checked';
+                 } else {
+                   var target = '.sn-in-progress';
+                   var checked = '';
+                 };
 
-              $(target).append(
-                `<div id="${id}" class="row justify-content-center">
-                  <div class="col-lg-1 col-md-1 col-sm-1 col-12 sn-check"><input type="checkbox" ${checked} data-id="${id}"></div>
-                  <div class="col-lg-3 col-md-5 col-sm-5 col-12 sn-name"><span class="align-middle">${name}</span></div>
-                  <div class="col-lg-2 col-md-2 col-sm-2 col-12 sn-desc"><span class="align-middle">${desc}</span></div>
-                  <div class="col-lg-2 col-md-2 col-sm-2 col-12 sn-vid"><span class="align-middle"><a target="_blank" href="${video}">Video</a></span></div>
-                </div>`);
-           });
-    }});
+                 $(target).append(
+                   `<div id="${id}" class="row justify-content-center">
+                     <div class="col-lg-1 col-md-1 col-sm-1 col-12 sn-check"><input type="checkbox" ${checked} data-id="${id}"></div>
+                     <div class="col-lg-3 col-md-5 col-sm-5 col-12 sn-name"><span class="align-middle">${name}</span></div>
+                     <div class="col-lg-2 col-md-2 col-sm-2 col-12 sn-desc"><span class="align-middle">${desc}</span></div>
+                     <div class="col-lg-2 col-md-2 col-sm-2 col-12 sn-vid"><span class="align-middle"><a target="_blank" href="${video}">Video</a></span></div>
+                   </div>`);
+              });
+       }});
+
+
 
   $('.container-fluid').on('click', 'input', function(){
     var row = $(this).closest('.row');
