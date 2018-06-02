69155fbcb9e20a47ce1797a185dfc5d063106a32
m1ndgap <posseslash@gmail.com>
2018-06-02 11:19:42 +0200

    finished perk tooltip

--- js/base.js
+++ js/base.js
@@ -10,4 +10,39 @@ $(document).ready(function() {
   $('.show-links').on('click', function(){
     $(this).next().toggle();
   });
+
+  $(".perk-wrap").on('mouseenter', function(){
+    var title = $(this).data('name');
+    var description = $(this).data('desc');
+    $(this).append(`<div class='perk-tooltip'><h5>${title}</h5>${description}</div>`);
+  });
+  $(".perk-wrap").on('mouseleave', function(){
+    $(this).find('.perk-tooltip').remove();
+  });
+
+    $(".perk-tooltip").on('mouseenter', function(){
+      $(this).remove();
+    });
+
+  // $(".perk-wrap").tooltip({
+  //     items: ".perk-wrap",
+  //     track: true,
+  //     content: function() {
+  //       var title = $(this).data('name');
+  //       var description = $(this).data('desc');
+  //       var content = `<h4>${title}</h4>`;
+        // if ( element.hasClass('perk-wrap') ) {
+        //   var text = element.text();
+        //   return "<img class='map' alt='" + text +
+        //     "' src='http://maps.google.com/maps/api/staticmap?" +
+        //     "zoom=11&size=350x350&maptype=terrain&sensor=false&center=" +
+        //     text + "'>";
+        // }
+        // if ( element.is( "[title]" ) ) {
+        //   return element.attr( "title" );
+        // }
+        // if ( element.is( "img" ) ) {
+        //   return element.attr( "alt" );
+  //     }
+  // });
 });
