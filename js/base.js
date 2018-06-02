35322bbebab803becfa43ea2322c267d329faaf2
m1ndgap <posseslash@gmail.com>
2018-06-02 18:48:29 +0200

    guns jsonp done, now working on ajax function

--- js/base.js
+++ js/base.js
@@ -20,31 +20,20 @@ $(document).ready(function() {
       jsonpCallback: 'callback',
         success: function (data) {
             console.log(data);
-            // var json = data;
-            // var localstorage = JSON.parse(localStorage.getItem('nodes'));
-            // $.each(json, function(index, value){
-            //    var id = json[index].id;
-            //    var name = json[index].name;
-            //    var desc = json[index].description;
-            //    var video = json[index].video;
-            //    if (localstorage.includes(id)) {
-            //      var target = '.sn-completed';
-            //      var checked = 'checked';
-            //      var completed = "1123";
-            //    } else {
-            //      var target = '.sn-in-progress';
-            //      var checked = '';
-            //      var completed = "131123";
-            //    };
-            //
-            //    $(target).append(
-            //      `<div id="${id}" class="row justify-content-center">
-            //        <div class="col-lg-1 col-md-1 col-sm-1 col-12 sn-check"><input type="checkbox" ${checked} data-id="${id}"></div>
-            //        <div class="col-lg-3 col-md-5 col-sm-5 col-12 sn-name"><span class="align-middle">${name}</span></div>
-            //        <div class="col-lg-2 col-md-2 col-sm-2 col-12 sn-desc"><span class="align-middle">${desc}</span></div>
-            //        <div class="col-lg-2 col-md-2 col-sm-2 col-12 sn-vid"><span class="align-middle"><a target="_blank" href="${video}">Video</a></span></div>
-            //      </div>`);
-            // });
+            var json = data;
+            $.each(json, function(index, value){
+               var id = json[index].id;
+               var name = json[index].name;
+               var desc = json[index].description;
+               var video = json[index].video;
+               $(target).append(
+                 `<div id="${id}" class="row justify-content-center">
+                   <div class="col-lg-1 col-md-1 col-sm-1 col-12 sn-check"><input type="checkbox" ${checked} data-id="${id}"></div>
+                   <div class="col-lg-3 col-md-5 col-sm-5 col-12 sn-name"><span class="align-middle">${name}</span></div>
+                   <div class="col-lg-2 col-md-2 col-sm-2 col-12 sn-desc"><span class="align-middle">${desc}</span></div>
+                   <div class="col-lg-2 col-md-2 col-sm-2 col-12 sn-vid"><span class="align-middle"><a target="_blank" href="${video}">Video</a></span></div>
+                 </div>`);
+            });
      }});
 
   $(".perk-wrap").on('mouseenter', function(){
